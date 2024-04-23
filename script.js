f3d333a98c6ed9fc7ade920e924492d6f12fe313
Kekwulugo <93893923+Kekwulugo@users.noreply.github.com>
2024-04-23 16:20:27 -0600

    Add API for WeatherAPI

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+async function getWeather(){
+  const response = await fetch ('http://api.weatherapi.com/v1/forecast.json?key=4e1ed8ad82bf447b940202722240804&q=calgary', {mode:'cors'})
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(response){
+      console.log(response);
+    })
+    .catch (e=> {
+      console.log(e)
+    })
+ }
 
