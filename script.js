f411f20243b801b76ce2a844bcf330405fcb4ff8
Kekwulugo <93893923+Kekwulugo@users.noreply.github.com>
2024-04-23 16:26:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 async function getWeather(){
-  const response = await fetch ('http://api.weatherapi.com/v1/forecast.json?key=4e1ed8ad82bf447b940202722240804&q=calgary', {mode:'cors'})
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(response){
-      console.log(response);
-    })
-    .catch (e=> {
-      console.log(e)
-    })
- }
+  const response = await fetch ('http://api.weatherapi.com/v1/forecast.json?key=4e1ed8ad82bf447b940202722240804&q=calgary', {mode:'cors'});
+  const weather = await response.json();
+  console.log(weather);
+}
 
