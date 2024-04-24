1712827958621e2a74ba98c35358665a42ff8a2e
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-24 16:59:38 -0600

    Add html structure

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 async function getWeather(){
-  const response = await fetch ('http://api.weatherapi.com/v1/forecast.json?key=4e1ed8ad82bf447b940202722240804&q=calgary', {mode:'cors'});
+  const response = await fetch ('http://api.weatherapi.com/v1/forecast.json?key=4e1ed8ad82bf447b940202722240804&q=calgary&days=5', {mode:'cors'});
   const weather = await response.json();
   console.log(weather);
 }
+
+getWeather();
 
