6b8231f9a7c245bc11a9e8921cd8abd666f52df4
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-05-01 14:14:50 -0600

    Add search feature

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-async function getWeather(){
-  const response = await fetch ('http://api.weatherapi.com/v1/forecast.json?key=4e1ed8ad82bf447b940202722240804&q=calgary&days=5', {mode:'cors'});
-  const weather = await response.json();
-  console.log(weather);
+let submitBtn = document.querySelector("button");
+console.log(submitBtn);
+let inputValue = document.querySelector("input").value;
+
+submitBtn.addEventListener('click', getWeather(inputValue));
+
+
+function renderApp(visibility, condition, uv, feelslike_c, feelslike_f, humidity, isDay, precipitation, tempC, tempF){
+
+  let humidityText = document.querySelector("#humidity-text");
+  humidityText.innerText = humidity;
 }
 
-getWeather();
-//we should call this every 10 minutes or something
+
+async function getWeather(city){
+  let url = 'http://api.weatherapi.com/v1/forecast.json?key=4e1ed8ad82bf447b940202722240804&days=5&q=' + city;
+
+  console.log(url);
+
+  const response = await fetch (url, {mode:'cors'});
+  const weather = await response.json();
+
+  console.log(weather);
+  
+  let visibility = weather.current.vis_km;
+  let condition = weather.current.condition.text;
+  let uv = weather.current.uv;
+  let feelslike_c = weather.current.feelslike_c;
+  let feelslike_f = weather.current.feelslike_f;
+  let humidity = weather.current.humidity;
+  let isDay = weather.current.is_day;
+  let precipitation = weather.current.precip_mm;
+  let tempC = weather.current.temp_c;
+  let tempF = weather.current.temp_f;
+
+  renderApp(visibility, condition, uv, feelslike_c, feelslike_f, humidity, isDay, precipitation, tempC, tempF);
+  
+}
+
+
+
+
 
